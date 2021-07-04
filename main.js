eb4a730cc6f4b5eda050e07a617b3c22560564e5
Jayant-tech967 <81297130+Jayant-tech967@users.noreply.github.com>
2021-07-04 22:01:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preLoad(){
+
+}
+function setup(){
+    canvas=createCanvas(640,480);
+    canvas.position(110,250);
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,640,480);
+    tint(tint_color);
+}
+function take_snapshot(){
+ save("image.png");
+}
+function filter_tilt(){
+    tint_color=document.getElementById("color_name").value;
+}
